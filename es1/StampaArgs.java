e5c7390356815aa21a2bf86c3808769574adf7e3
edoardo-claps <edoardo.claps@gmail.com>
2022-02-18 11:12:09 +0100

    aggiunta main e util

--- es1/StampaArgs.java
+++ es1/StampaArgs.java
@@ -3,8 +3,7 @@
 import java.util.Scanner;
 
 public class StampaArgs {
-    // per il confronto utilizzare in un ciclo substring oppure charAt
-    // metodo 2 (non lo avevo visto)
+
     public static void main(String[] args) {
 
 
